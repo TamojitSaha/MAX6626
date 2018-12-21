46c8d4798e6c107d63e451097ff38c628b48759f
Tamojit Saha <sahatamo1995@gmail.com>
2018-12-21 16:15:36 +0530

    Added support for ATtiny45 and ATtiny85

--- examples/MAX6626_test.ino
+++ examples/MAX6626_test.ino
@@ -8,7 +8,17 @@
     This is a demo sketch for interfacing with MAX6266
 
     This sketch assumes that ADDR pin is conncted to GND.
+
+    UPDATE: 
+    21 Dec 2018
+      - Added support for ATtiny 85 and ATtiny 45
 */
+
+#if defined(__AVR_ATtiny85__) || defined (__AVR_ATtiny45__)
+#define __NO_DEBUG__
+#else
+#define __DEBUG__
+#endif
 
 #include "MAX6626.h"
 
@@ -17,13 +27,17 @@ MAX6626 tempSensor = MAX6626();
 
 void setup()
 {
+#ifdef __DEBUG__
   Serial.begin(9600);
+#endif
   //tempSensor.begin(ADDR_VCC)
   //tempSensor.begin(ADDR_SCL)
   //tempSensor.begin(ADDR_SDA)
   if (!tempSensor.begin())    //Default: tempSensor.begin(ADDR_GND)
   {
+#ifdef __DEBUG__
     Serial.println("MAX6626 not found!");
+#endif
     while (1);
   }
 }
@@ -37,10 +51,11 @@ void loop()
   //Read and print the temperature
   float c = tempSensor.readTemp();          //returns value in deg C
   float f = c * 9.0 / 5.0 + 32;             //convert deg C to deg F
-  Serial.print("Temp: ");Serial.print(c,4); //print 4 point decimal value
-  Serial.print("*C\t");Serial.print(f,4);
+#ifdef __DEBUG__
+  Serial.print("Temp: "); Serial.print(c, 4); //print 4 point decimal value
+  Serial.print("*C\t"); Serial.print(f, 4);
   Serial.println("*F");
-
+#endif
   //Serial.println("Shutdown MAX6626");
   //tempSensor.sleep();
 
